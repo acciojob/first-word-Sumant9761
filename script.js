c0311328d19756c93bfecfa2ecfff58a0f013606
Sumant9761 <142734516+Sumant9761@users.noreply.github.com>
2026-05-30 14:42:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+
+	let index = s.indexOf(" ");
+
+	if(index == -1){
+		return s;
+	}
+
+	return s.slice(0, index);
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s));  
